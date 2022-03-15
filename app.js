b556b931f60de132e5bc856b5f7bc2d2ac35d2b9
Phuc Trinh <trinhgiaphuc2k@gmail.com>
2022-03-15 19:15:33 +0700

    init

---
+++ app.js
@@ -0,0 +1,51 @@
+const video = document.querySelector('#video');
+const acceptBtn = document.querySelector('#cool');
+const rejectBtn = document.querySelector('#no');
+const fuckOffBtn = document.querySelector('#fuckoff');
+
+function handleAccept() {
+  console.log('Clicked');
+}
+function handleReject() {
+  console.log('Clicked');
+}
+function handleFuckOff() {
+  console.log('Clicked');
+}
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  // faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  // faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+]).then(() => {
+  console.log('Face Regconize Is Ready');
+  startVideo();
+});
+
+rejectBtn.addEventListener('click', handleAccept);
+fuckOffBtn.addEventListener('click', handleReject);
+acceptBtn.addEventListener('click', handleFuckOff);
+
+function startVideo() {
+  // navigator.getUserMedia(
+  //   { video: {} },
+  //   stream => (video.srcObject = stream),
+  //   err => console.error(err)
+  // );
+}
+
+// video.addEventListener('play', () => {
+// const canvas = faceapi.createCanvasFromMedia(video);
+// const { width, height } = video;
+// faceapi.matchDimensions(canvas, { width, height });
+// document.body.append(canvas);
+// setInterval(async () => {
+//   const result = await faceapi
+//     .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+//     .withFaceLandmarks();
+//   const resizeDetections = faceapi.resizeResults(result, { width, height });
+//   canvas.getContext('2d', { width, height }).clearRect(0, 0, width, height);
+//   faceapi.draw.drawDetections(canvas, resizeDetections);
+// }, 100);
+// });

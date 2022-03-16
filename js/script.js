88581c68a8da28edf91f5ce81459da4a627e20a0
Phuc Trinh <trinhgiaphuc2k@gmail.com>
2022-03-16 13:18:06 +0700

    complete scene1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Utils Function
 function deepRemove(node) {
   if (node.childNodes.length > 0) {
     node.childNodes.forEach(deepRemove);
@@ -6,6 +7,7 @@ function deepRemove(node) {
   // Move node.remove() down here and it's gonna fuck everything up
 }
 
+// Scene1
 function handleAccept() {
   msg.innerHTML = "Say ok when you're ready 👌";
 
@@ -15,24 +17,64 @@ function handleAccept() {
   scene.append(takingPicture);
 }
 function handleReject() {
-  msg1.innerHTML = 'Well...Bye then 🤷‍♂️';
+  msg.innerHTML = 'Well...Bye then 🤷‍♂️';
+
   answerWrapper.setAttribute('hidden', 'true');
   setTimeout(() => {
-    bicyle.classList = 'leaving';
+    bicyle.classList = 'img leaving-5s';
     setTimeout(() => {
       chatbox.setAttribute('hidden', 'true');
       bicyle.setAttribute('hidden', 'true');
-    }, 1000);
+    }, 5000);
   }, 1500);
 }
 function handleFuckOff() {
-  console.log('Clicked');
+  bicyle.classList = 'img leaving-1s';
+  setTimeout(() => {
+    chatbox.toggleAttribute('hidden');
+    bicyle.toggleAttribute('hidden');
+
+    setTimeout(() => {
+      bicyle.classList = 'img coming-1s';
+      bicyle.toggleAttribute('hidden');
+
+      setTimeout(() => {
+        answerWrapper.remove();
+        msg.innerHTML = 'Oh. I forgot somthing!';
+        chatbox.toggleAttribute('hidden');
+        chatbox.classList = 'chat-box';
+        setTimeout(() => {
+          bicyle.toggleAttribute('hidden');
+          setTimeout(() => {
+            bicyle.toggleAttribute('hidden');
+            bicyle.src = '/assets/fuckyou.svg';
+            bicyle.classList = 'img coming-1s';
+            setTimeout(() => {
+              bicyle.classList = 'img leaving-1s';
+              setTimeout(() => {
+                bicyle.src = 'assets/guy-cycling.svg';
+                bicyle.classList = 'img';
+                setTimeout(() => {
+                  bicyle.classList = 'img leaving-1s';
+                  setTimeout(() => {
+                    bicyle.toggleAttribute('hidden');
+                    chatbox.toggleAttribute('hidden');
+                  }, 1000);
+                }, 50);
+              }, 1000);
+            }, 1000);
+          }, 0);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
 }
 
 acceptBtn.addEventListener('click', handleAccept);
 rejectBtn.addEventListener('click', handleReject);
 fuckOffBtn.addEventListener('click', handleFuckOff);
 
+// Scene2
 function howItWork() {
   okBtn.innerHTML = "I got this now ✨. Let's go take some pictures";
   fraudBtn.classList = 'answer';
@@ -44,6 +86,7 @@ function howItWork() {
   answerWrapper2.removeChild(howItWorkBtn);
   answerWrapper2.append(fraudBtn);
 }
+
 function ready() {
   scene2.removeChild;
 }
